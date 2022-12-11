a51a4be251934c33f826a296077c1511d2685654
Iain Cambridge <iain@humblyarrogant.io>
2022-12-11 15:07:29 +0100

    Update readme and license

--- src/Export/Compiler/ExportCompilerPass.php
+++ src/Export/Compiler/ExportCompilerPass.php
@@ -5,7 +5,18 @@ declare(strict_types=1);
 /*
  * Copyright Iain Cambridge 2020-2022.
  *
- * Use of this software is governed by the GPL V3 License. As found in the LICENSE file in the root or at https://github.com/getparthenon/export-bundle/LICENSE
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 namespace Parthenon\Export\Compiler;
