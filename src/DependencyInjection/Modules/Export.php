898b071368728374bc75ff20dce7938677a33c82
Iain <iain@humblyarrogant.io>
2023-03-26 14:10:41 +0000

    [MultiTenancy] check if enabled parameter exists
    Automatic commit for https://github.com/getparthenon/monorepo/commit/8ac0a3b95b315b5451486e43a5405d21ab74fe4e

--- src/DependencyInjection/Modules/Export.php
+++ src/DependencyInjection/Modules/Export.php
@@ -58,8 +58,6 @@ class Export implements ModuleConfigurationInterface
             return;
         }
 
-        $container->setParameter('parthenon_export_enabled', true);
-
         $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
         $loader->load('services/export.xml');
 
