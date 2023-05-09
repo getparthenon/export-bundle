ee39f3baed4c2a53c2388bd049f69f2a54cf931c
Iain <iain@humblyarrogant.io>
2023-05-09 10:53:06 +0000

    [Notification] Add logging to PostMarkSender
    Automatic commit for https://github.com/getparthenon/monorepo/commit/c9778f523da2f4165d7ef7b719a677e014055f1c

--- src/Notification/Sender/PostmarkEmailSender.php
+++ src/Notification/Sender/PostmarkEmailSender.php
@@ -21,6 +21,7 @@ declare(strict_types=1);
 
 namespace Parthenon\Notification\Sender;
 
+use Parthenon\Common\LoggerAwareTrait;
 use Parthenon\Notification\EmailInterface;
 use Parthenon\Notification\EmailSenderInterface;
 use Parthenon\Notification\Exception\UnableToSendMessageException;
@@ -29,6 +30,8 @@ use Postmark\PostmarkClient;
 
 final class PostmarkEmailSender implements EmailSenderInterface
 {
+    use LoggerAwareTrait;
+
     public function __construct(private PostmarkClient $postmarkClient)
     {
     }
@@ -55,7 +58,10 @@ final class PostmarkEmailSender implements EmailSenderInterface
                 $email['HtmlBody'] = $message->getContent();
                 $response = $this->postmarkClient->sendEmailBatch([$email]);
             }
+
+            $this->getLogger()->info('Sent email using PostMark');
         } catch (\Exception $e) {
+            $this->getLogger()->warning('Unable to send email using PostMark', ['exception_message' => $e->getMessage()]);
             throw new UnableToSendMessageException($e->getMessage(), $e->getCode(), $e);
         }
     }
