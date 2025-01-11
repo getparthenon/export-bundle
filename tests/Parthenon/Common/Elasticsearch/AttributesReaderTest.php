0ef533c947ee7f865dbadd2dd6f97f611d5edbb5
Iain <iain@humblyarrogant.io>
2025-01-11 22:38:35 +0000

    Update loading message
    Automatic commit for https://github.com/getparthenon/monorepo/commit/e253e261f3cd74c6e6ee076a8951f50ac5885c4a

--- tests/Parthenon/Common/Elasticsearch/AttributesReaderTest.php
+++ tests/Parthenon/Common/Elasticsearch/AttributesReaderTest.php
@@ -3,17 +3,17 @@
 declare(strict_types=1);
 
 /*
- * Copyright Iain Cambridge 2020-2022.
+ * Copyright (C) 2020-2025 Iain Cambridge
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
@@ -29,7 +29,7 @@ class AttributesReaderTest extends TestCase
 {
     public function testFindsAttributeOnClass()
     {
-        $object = new class() {
+        $object = new class {
             #[Field(name: 'field_name')]
             private string $field;
         };
@@ -47,7 +47,7 @@ class AttributesReaderTest extends TestCase
     {
         $this->expectException(InvalidAttibuteException::class);
 
-        $object = new class() {
+        $object = new class {
             #[Field()]
             private string $field;
         };
--- tests/Parthenon/Export/Notification/ExportEmailFactoryTest.php
+++ tests/Parthenon/Export/Notification/ExportEmailFactoryTest.php
@@ -3,17 +3,17 @@
 declare(strict_types=1);
 
 /*
- * Copyright Iain Cambridge 2020-2022.
+ * Copyright (C) 2020-2025 Iain Cambridge
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * it under the terms of the GNU LESSER GENERAL PUBLIC LICENSE as published by
+ * the Free Software Foundation, either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
@@ -31,7 +31,7 @@ class ExportEmailFactoryTest extends TestCase
 {
     public function testEmail()
     {
-        $user = new class() implements UserInterface {
+        $user = new class implements UserInterface {
             public function getRoles(): array
             {
                 return [];
